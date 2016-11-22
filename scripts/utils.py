7602dadc817bc2c9c73dac7bfe908c91d698d992
vishakhhegde <vishakh@stanford.edu>
2016-11-22 16:25:11 -0500

    changing abstractions

---
+++ scripts/utils.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+import numpy as np
+import sys, os
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev = 0.01)
+    return tf.Variable(initial)
+
+def bias_variable(shape):
+    initial = tf.constant(0.01, shape = shape)
+    return tf.Variable(initial)
+
+def conv2d(x, W, stride):
+    return tf.nn.conv2d(x, W, strides = [1, stride, stride, 1], padding = "SAME")
+
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = "SAME")
