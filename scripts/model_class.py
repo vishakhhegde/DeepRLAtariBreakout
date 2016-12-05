0bdcb8de29a86bf8e143dc39dd6905b1485edbd0
Ubuntu <ubuntu@ip-172-31-39-97.us-west-2.compute.internal>
2016-12-05 03:39:29 +0000

    pause while rendering

--- scripts/model_class.py
+++ scripts/model_class.py
@@ -245,7 +245,7 @@ class deepRL_model():
 		while i < NUM_TEST_GAMES:
 			if render:
 				game_state.render()
-				sleep(0.001)
+				sleep(0.005)
 			if not TEST_EPSILON == 1.0:
 				readout_t = Qvalues.eval(feed_dict = {s : [s_t]})[0]
 				if random.random() <= TEST_EPSILON:
